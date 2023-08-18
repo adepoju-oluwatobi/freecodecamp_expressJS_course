471aeec48e41c3f67c690677dadb60003a25f484
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-18 09:23:22 +0100

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -3,11 +3,18 @@ let app = express();
 console.log("Hello World");
 
 const absolutePath = __dirname + "/views/index.html";
+const path = __dirname + "/public";
 
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);
 })
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(path));
+
+app.get('/json', (req, res) => {
+    res.json({
+        message: "Hello json"
+    })
+})
 
 
 
