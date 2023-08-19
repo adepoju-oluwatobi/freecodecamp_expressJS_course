a6a8e580d81ba2b724c0e329b0c183be861151c0
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-08-19 10:18:34 +0100

    fixed the .env project

--- myApp.js
+++ myApp.js
@@ -12,14 +12,13 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(path));
 
 app.get('/json', (req, res) => {
-   const messageStyle = process.env.MESSAGE_STYLE;
-   let message = "Hello json";
-
-   if (messageStyle === "uppercase"){
-    message = message.toUpperCase();
-   }
-
-   res.json({ message: message });
+    var response = "Hello json".toUpperCase();
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "Hello json".toUpperCase();
+  } else {
+    response = "Hello json";
+  }
+   res.json({ message: response });
 });
 
 
